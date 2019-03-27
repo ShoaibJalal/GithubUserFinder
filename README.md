e591ff8be662f1c11ad64bde09074108d1f1616a
shoaibjalal <shoaib.jalal@myy.haaga-helia.fi>
2019-03-27 13:53:27 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+This GitHub user finder app is built with Vanilla_JavaScript using object oriented programming. UI of this app is done with Bootswatch css. 
+User can make a request to github api to search for other users. This app is using promises and callbacks plus "async and await". "fetch" is used to get the data from github api.
+
+
+To run the project, Download or clone it on your machine and open index.html on your browser.
+
+<img src="img/screenshot.PNG" />
+
---
+++ github.js
@@ -0,0 +1,22 @@
+class Github {
+  constructor() {
+    this.client_id = '83fdb176e0398a919d2a';
+    this.client_secret = 'd7d9d103f5c6e7b45600d62751a0a508f0c387dc';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
